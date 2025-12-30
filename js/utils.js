08d7dc02043866b5ef4d506325a6281f8e8e0bf8
Azpald <fathost22@gmail.com>
2025-12-30 22:15:50 +0700

    Test features

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,14 @@
-function cubicBezier(t, p1, p2, p3, p4) {
-  // Clamp input to [0, 1]
+function cubicBezier(t, p0, p1, p2, p3) {
+  // Clamp t to the range [0, 1]
   t = Math.max(0, Math.min(1, t));
 
   const u = 1 - t;
 
   return (
-    u * u * u * p1 +
-    3 * u * u * t * p2 +
-    3 * u * t * t * p3 +
-    t * t * t * p4
+    u * u * u * p0 +          // Start point
+    3 * u * u * t * p1 +      // First control point
+    3 * u * t * t * p2 +      // Second control point
+    t * t * t * p3            // End point
   );
 }
 
@@ -35,4 +35,107 @@ function rgbToHex(rgb) {
     return (component % 256).toString(16).padStart(2, '0');
   }).join('');
   return `#${hex}`;
+}
+
+
+function monochrome4(gray, x, y) {
+  const blockSize = 2;
+  const blocks = [
+    [
+      [0, 0],
+      [0, 0],
+    ],
+    [
+      [0, 0],
+      [0, 1],
+    ],
+    [
+      [1, 0],
+      [0, 1],
+    ],
+    [
+      [1, 1],
+      [0, 1],
+    ],
+    [
+      [1, 1],
+      [1, 1],
+    ],
+  ];
+  const blockId = Math.floor(gray/255 * 5);
+
+  try {
+    // let block = blocks[blockId];
+    // if (y % 4 < 2) block = block.reverse();
+    // let blockRow = block[y % blockSize];
+    // if (x % 4 < 2) blockRow = blockRow.reverse();
+    // return blockRow[x % blockSize] * 255;
+
+    return blocks[blockId][y % blockSize][x % blockSize] * 255;
+  } catch (error) {
+    return 0;
+  }
+}
+
+function monochrome9(gray, x, y) {
+  const blockSize = 3;
+  const blocks = [
+    [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ],
+    [
+      [0, 0, 0],
+      [0, 1, 0],
+      [0, 0, 0],
+    ],
+    [
+      [0, 0, 0],
+      [1, 0, 0],
+      [0, 0, 1],
+    ],
+    [
+      [0, 1, 0],
+      [0, 0, 1],
+      [1, 0, 0],
+    ],
+    [
+      [0, 1, 0],
+      [1, 0, 1],
+      [0, 1, 0],
+    ],
+    [
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 1, 0],
+    ],
+    [
+      [1, 1, 0],
+      [1, 0, 1],
+      [0, 1, 1],
+    ],
+    [
+      [1, 1, 0],
+      [1, 1, 1],
+      [0, 1, 1],
+    ],
+    [
+      [1, 1, 1],
+      [1, 0, 1],
+      [1, 1, 1],
+    ],
+    [
+      [1, 1, 1],
+      [1, 1, 1],
+      [1, 1, 1],
+    ],
+  ];
+  const blockId = Math.floor(gray/255 * 10);
+
+  try {
+    return blocks[blockId][y % blockSize][x % blockSize] * 255;
+  } catch (error) {
+    return 0;
+  }
 }
